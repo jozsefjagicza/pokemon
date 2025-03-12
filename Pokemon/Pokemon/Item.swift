0908938e31151a95dbbbe9fd3803dcf225425ad5
József Jagicza <jozsefjagicza@icloud.com>
2025-03-12 13:14:58 +0100

    init commit

---
+++ Pokemon/Pokemon/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Pokemon
+//
+//  Created by József Jagicza on 2025. 03. 12..
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ Pokemon/PokemonTests/PokemonTests.swift
@@ -0,0 +1,16 @@
+//
+//  PokemonTests.swift
+//  PokemonTests
+//
+//  Created by József Jagicza on 2025. 03. 12..
+//
+
+import Testing
+
+struct PokemonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
